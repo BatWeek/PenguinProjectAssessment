c477906aee4a6d6ea18a870c57aa35e181647ac5
BatWeek <sebastian.evans@stcatz.ox.ac.uk>
2024-12-11 12:16:57 +0000

    Ready for sharing again

--- functions/PenguinFunctions.R
+++ functions/PenguinFunctions.R
@@ -1,3 +1,20 @@
+#This function cleans the penguins data.
+clean.penguin.data <- function(data){
+  data %>%
+  clean_names() %>%
+  select(-comments) %>%
+  select(-starts_with("delta")) %>%
+  select(-stage) %>%
+  select(-individual_id) %>%
+  select(-clutch_completion) %>%
+  select(-date_egg) %>%
+  mutate(species = case_when(
+    species == "Adelie Penguin (Pygoscelis adeliae)" ~ "Adelie",
+    species == "Chinstrap penguin (Pygoscelis antarctica)" ~ "Chinstrap",
+    species == "Gentoo penguin (Pygoscelis papua)" ~ "Gentoo")) %>%
+  na.omit()
+}
+
 
 
 #This is a function to save figures as an svg file. The arguments are as follows:
@@ -14,3 +31,7 @@ save.as.svg <- function(plot, name, x, y, scaling) {
   print(plot)
   dev.off()
 }
+
+
+
+
